b4cfadb4b70ae3ae48c0789fc4fed3fa53c0bc40
lianxf <920259310@qq.com>
2018-06-09 18:46:58 +0800

    11

---
+++ src/main/java/cn/ananyz/cp/service/mapper/CpDataMapper.java
@@ -0,0 +1,7 @@
+package cn.ananyz.cp.service.mapper;
+
+import cn.ananyz.cp.service.model.CpData;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface CpDataMapper extends Mapper<CpData> {
+}
---
+++ src/main/java/cn/ananyz/cp/service/mapper/CpParityAnalysisMapper.java
@@ -0,0 +1,7 @@
+package cn.ananyz.cp.service.mapper;
+
+import cn.ananyz.cp.service.model.CpParityAnalysis;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface CpParityAnalysisMapper extends Mapper<CpParityAnalysis> {
+}
