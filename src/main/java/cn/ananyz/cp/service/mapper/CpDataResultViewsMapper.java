8d9227d4c2aeeb1408c029722eb921613d793344
lianxf <920259310@qq.com>
2018-06-22 18:41:13 +0800

    11

---
+++ src/main/java/cn/ananyz/cp/service/mapper/CpDataResultViewsMapper.java
@@ -0,0 +1,7 @@
+package cn.ananyz.cp.service.mapper;
+
+import cn.ananyz.cp.service.model.CpDataResultViews;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface CpDataResultViewsMapper extends Mapper<CpDataResultViews> {
+}
