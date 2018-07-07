d50ce250bfe5fb551a57dbb30e113fa23fb830a9
lianxf <920259310@qq.com>
2018-07-07 19:20:30 +0800

    11

--- src/main/resources/db/db.sql
+++ src/main/resources/db/db.sql
@@ -85,3 +85,36 @@ CREATE TABLE `cp_parity_analysis` (
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+
+DROP TABLE IF EXISTS `cp_data_result_ssc_tj`;
+
+CREATE TABLE `cp_data_result_ssc_tj` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `cp_date` varchar(10) DEFAULT NULL COMMENT '日期',
+  `cp_qi_hao` varchar(3) DEFAULT NULL COMMENT '期号',
+  `cp_index` enum('1','2','3','4','5') DEFAULT NULL COMMENT '位置',
+  `cp_num` enum('0','1','2','3','4','5','6','7','8','9') DEFAULT NULL COMMENT '号码',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11761 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `cp_data_result_views_ssc_tj` */
+
+DROP TABLE IF EXISTS `cp_data_result_views_ssc_tj`;
+
+CREATE TABLE `cp_data_result_views_ssc_tj` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `cp_index` enum('1','2','3','4','5') DEFAULT NULL COMMENT '角标',
+  `end_qi_hao` varchar(3) DEFAULT NULL COMMENT '结束期号',
+  `start_qi_hao` varchar(3) DEFAULT NULL COMMENT '开始期号',
+  `cru_hao_ma` varchar(1) DEFAULT NULL COMMENT '当前号码',
+  `cishu` int(3) DEFAULT NULL COMMENT '次数',
+  `yichu` varchar(32) DEFAULT NULL COMMENT '已出号码',
+  `weichu` varchar(32) DEFAULT NULL COMMENT '未出号码',
+  `create_time` timestamp NULL DEFAULT NULL COMMENT '采集时间',
+  `create_date` date DEFAULT NULL COMMENT '采集日期',
+  `start_qi_date` date DEFAULT NULL COMMENT '开始期号开奖日期',
+  `end_qi_date` date DEFAULT NULL COMMENT '结束期号开奖日期',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7209 DEFAULT CHARSET=utf8;
