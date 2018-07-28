2cd886ac9c3d38510306220069746f5e2735dd11
lianxf <920259310@qq.com>
2018-07-28 17:29:00 +0800

    11

---
+++ src/main/java/cn/ananyz/cp/service/data/collection/parse/CpApiXj.java
@@ -0,0 +1,4 @@
+package cn.ananyz.cp.service.data.collection.parse;
+
+public interface CpApiXj extends CpApi{
+}
---
+++ src/main/java/cn/ananyz/cp/service/data/collection/parse/impl/CpApiXjImpl.java
@@ -0,0 +1,78 @@
+package cn.ananyz.cp.service.data.collection.parse.impl;
+
+import cn.ananyz.cp.service.data.collection.model.CPDataModel;
+import cn.ananyz.cp.service.data.collection.parse.CpApi500;
+import cn.ananyz.cp.service.data.collection.parse.CpApiXj;
+import cn.ananyz.cp.service.utils.DateUtil;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+@Component
+public class CpApiXjImpl extends BaseCpApiImpl implements CpApiXj {
+
+    @Override
+    public CPDataModel getTodayLastData(Date date) throws IOException, ParseException {
+        return super.getTodayLastData(date);
+    }
+
+    @Override
+    public CPDataModel getDataByDateAndQiHao(Date date, Integer qiHao) throws IOException, ParseException {
+        return super.getDataByDateAndQiHao(date, qiHao);
+    }
+
+    private Document getTodayDocument(Date date) throws IOException {
+        Document doc = Jsoup.connect("http://www.xjflcp.com/game/sscIndex").get();
+        return doc;
+    }
+    private Elements getTodayElements(Document document){
+        Element jq_body_kc_result = document.getElementById("jq_body_kc_result");
+        Elements tr = jq_body_kc_result.getElementsByTag("tr");
+        return tr;
+    }
+
+    public static void main(String[] args) throws IOException, ParseException {
+        CpApiXjImpl cpApiXj = new CpApiXjImpl();
+//        List<CPDataModel> todayAllData = cpApi500.getTodayAllData(new Date());
+        System.out.println(cpApiXj.getTodayDocument(new Date()));
+    }
+    @Override
+    public List<CPDataModel> getTodayAllData(Date date) throws IOException, ParseException {
+        ArrayList<CPDataModel> cpDataModels = new ArrayList<CPDataModel>();
+        Document document = getTodayDocument(date);
+        Elements todayElements = getTodayElements(document);
+        Iterator<Element> iterator = todayElements.iterator();
+        while (iterator.hasNext()){
+            Element next = iterator.next();
+            Elements td = next.getElementsByTag("td");
+
+            String dataWinNumber = td.get(2).text();
+            String dataPeriod = td.get(0).text();
+            String opentime = td.get(1).text() + ":00";
+            String qiHao = "0" + dataPeriod.substring(8,dataPeriod.length()-1);
+
+            CPDataModel cPDataModel = new CPDataModel();
+            cPDataModel.setLongDateAndQiHao(dataPeriod.substring(0,dataPeriod.length()-1));
+            cPDataModel.setShortQiHao(qiHao);
+            cPDataModel.setOpenTime(DateUtil.parseDate(opentime,DateUtil.PATTERN_DATE_TIME));
+
+            String[] split = dataWinNumber.trim().split(",");
+            setOpenNum(cPDataModel, split);
+            cpDataModels.add(cPDataModel);
+        }
+        return cpDataModels;
+    }
+
+
+
+
+}
